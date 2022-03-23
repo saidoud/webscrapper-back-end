689e959791d7324d4a0a324ea5947595d7f239c0
saidoud <develop.softoud@gmail.com>
2022-03-23 14:54:41 +0100

    hover category

--- routes/scrapper.js
+++ routes/scrapper.js
@@ -6,13 +6,16 @@ const puppeteer = require('puppeteer-extra')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
+// url page
+const url = 'https://www.mediamarkt.es';
+
 
 /* GET Scrapper page. */
 router.get('/', function (req, res, next) {
     puppeteer.launch({ headless: false }).then(async browser => {
         console.log('Running tests..')
         const page = await browser.newPage()
-        await page.goto('https://www.mediamarkt.es', { waitUntil: "networkidle2" })
+        await page.goto(url, { waitUntil: "networkidle2" })
 
         // Accept Cookie
         await page.click("#pwa-consent-layer-accept-all-button");
@@ -21,9 +24,13 @@ router.get('/', function (req, res, next) {
         // Click Category
         await page.click("#mms-app-header-category-button");
         await page.waitForTimeout(4335)
+
+        // Hover Category
+        await page.hover('a[aria-label="Informática"]')
+
         // await browser.close()
     }).catch(() => {
-        console.log("Error")
+        console.log("Somthing Wrong")
     })
 
     res.send('respond with a resource');
